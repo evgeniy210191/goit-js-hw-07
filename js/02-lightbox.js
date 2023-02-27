c5423430c799ef05428d6e8f839399a9982ff8b6
evgeniy210191 <bees127@ukr.net>
2023-02-27 21:56:05 +0200

    f

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,57 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-console.log(galleryItems);
 const galleryContainer = document.querySelector('.gallery');
 
 function displayListGallery(listGallery) {
   return listGallery
     .map((item) => {
       return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${item.original}">
-        <img class="gallery__image"
-          src="${item.preview}" 
-          alt="${item.description}" 
-        />
-      </a>
-    </li>
-    `;
+        <a class="gallery__item" href="${item.original}">
+          <img class="gallery__image" 
+            src="${item.preview}" 
+            alt="${item.description}"
+           
+          />
+        </a>
+      `;
     })
     .join('');
 }
-
-
 
 const showGallery = displayListGallery(galleryItems);
 
 function openModal(event) {
   event.preventDefault();
+
   if (event.target.className !== 'gallery__image') {
     return;
   }
-var lightbox = new SimpleLightbox('.gallery a', {
-  /* options */
+  
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  scrollZoom: false,
+  disableScroll: true,
+  captionDelay: 250,
+  captions: true,
+  captionsData: 'alt',
 });
-
-  window.addEventListener('keydown', closeModalEsc);
-}
-
-function closeModalEsc(event) {
-  event.preventDefault();
-  const box = document.querySelector('.basicLightbox');
-
-  if (!box) {
-    return;
-  }
-
-  if (event.code === 'Escape') {
-    box.classList.remove('basicLightbox--visible');
-    setTimeout(() => document.body.removeChild(box), 400);
-  }
-
-  window.removeEventListener('keydown', closeModalEsc);
+  console.log(lightbox);
+  
+lightbox.on('show.simplelightbox', function (e) {
+  document.querySelector('body').removeAttribute('style')
+});
 }
 
 galleryContainer.insertAdjacentHTML('beforeEnd', showGallery);
-
 galleryContainer.addEventListener('click', openModal);
